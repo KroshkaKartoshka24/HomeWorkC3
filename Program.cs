71fcd71706c8bfe6aa755b7d7fed75e2127c945e
Christina <nemovakg@gmail.com>
2023-10-11 19:25:56 +0300

    Задача 36

--- Program.cs
+++ Program.cs
@@ -2,17 +2,37 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+// System.Console.WriteLine("Введите размер массива ");
+// int a = Convert.ToInt32(System.Console.ReadLine());
+// int[] rndArr = new int[a];
+// int ch = 0;
+// for (int i = 0; i < a; i++)
+// {
+//     rndArr[i] = new Random().Next(100,999);
+//     Console.Write(rndArr[i] + " ");
+//     if (rndArr[i]%2 == 0)
+//     {
+//         ch++;
+//     }
+// }
+// Console.WriteLine($"\nКоличество четных чисел в массиве = {ch}");
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
 System.Console.WriteLine("Введите размер массива ");
 int a = Convert.ToInt32(System.Console.ReadLine());
 int[] rndArr = new int[a];
-int ch = 0;
+int nech = 0;
 for (int i = 0; i < a; i++)
 {
-    rndArr[i] = new Random().Next(100,999);
+    rndArr[i] = new Random().Next(1,9);
     Console.Write(rndArr[i] + " ");
-    if (rndArr[i]%2 == 0)
+    if ((i+1)%2 > 0)
     {
-        ch++;
+        nech = nech + rndArr[i];
     }
 }
-Console.WriteLine($"\nКоличество четных чисел в массиве = {ch}");
+Console.WriteLine($"\nСумму элементов, стоящих на нечётных позициях = {nech}");
